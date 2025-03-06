87641d02194f78627be507eece306b32933a19e2
Anirban Basak <jeetbasak2002@gmail.com>
2025-03-06 10:25:55 +0530

    feat : Configured the App Level Routing and added the styles snippet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+// These are the routes for the App Module
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
